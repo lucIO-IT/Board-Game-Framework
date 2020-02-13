8edc7648e6b32f4fbd98160cd31ef65b480a042a
Emanuele <e.am.romagnoli@gmail.com>
2020-02-13 18:02:36 +0100

    fixed engine

--- engine/engine.js
+++ engine/engine.js
@@ -1,4 +1,5 @@
 import {GameElement} from './core/core.js';
+import {Faction, Region} from './core/models.js';
 import {settings} from './settings/settings.js';
 import {GameMap} from './component/game_map.js';
 import * as widgets from './component/widgets.js';
@@ -8,13 +9,35 @@ import {regions} from './regions.js';
 class GameEngine {
     constructor(app){
         this.app = app;
-        this.factions = factions;
-        this.regions = regions;
+        this.settings = settings;
+        this.factions = this.__loadData__('factions');
+        this.regions = this.__loadData__('regions');;
         this.actions = {};
         this.start = this.__create_game__();
     }
 
+    __loadData__(name){
+        let obj = new Object();
+        //const args = JSON.parse(json).name;
+        const args = this.settings[name];
+        Object.keys(args).forEach(key => {
+            switch(name){
+              case 'regions':
+                obj[key] = new Region(args[key]);
+                break
+              case 'factions':
+                obj[key] = new Faction(args[key]);
+                break
+
+            }
+        });
+
+        return obj;
+    }
+
     __create_game__(){
+        console.log(this.factions);
+        console.log(this.regions);
         const map = document.createElement('game-map');
         Object.keys(settings.map).forEach(key => {
             map.setAttribute(key, settings.map[key])
--- engine/settings/settings.js
+++ engine/settings/settings.js
@@ -1,11 +1,11 @@
 const settings = {
     map: {
-      width: "850",
-      height: "600"
+      width: "1150",
+      //height: "600"
     },
     panel_faction: {
-        width: "850",
-        height: "150",
+        width: "1150",
+        //height: "150",
         slot: "panel-faction",
         details: '{"name":"Kingdom of Sicily","symbol":"sc/symbols/sicily.png"}'
     },
@@ -15,6 +15,95 @@ const settings = {
         details: '{"this": "this"}',
         url: "",
         style: "display: none; "
+    },
+    factions: {
+        hre: {
+            name: 'Regno di Germania',
+            royal_family: {name: 'Otto', age: 24, sons: [{name: 'Otto', age: 4, sons: []}, {name: 'Heinrich', age: 2, sons: []}]},
+            provinces: ['IT-23', 'IT-21', 'IT-42'],
+            color: 'black',
+            symbol: 'sc/ad.png',
+            money: 0,
+            rank: 1,
+            human: false
+        },
+        italy: {
+            name: 'Regno di Italia',
+            royal_family: {name: 'Berengar', age: 36, sons: []},
+            provinces: ['IT-25', 'IT-32'],
+            color: 'green',
+            symbol: 'sc/ad.png',
+            money: 0,
+            rank: 1,
+            human: false
+        }
+    },
+    regions: {
+        'IT-23': {
+            name: 'Aosta',
+            capital: 'Aosta',
+            income: 2,
+            population: 10,
+            neighbours: ['IT-21'],
+            army: 0,
+            garrison: 50,
+            troops: 50,
+            defence: 1,
+            structures: 1,
+            image: 'sc/terrains/terrain.jpg'
+        },
+        'IT-21': {
+            name: 'Piemonte',
+            capital: 'Torino',
+            income: 7,
+            population: 35,
+            neighbours: ['IT-23', 'IT-42', 'IT-25'],
+            army: 0,
+            garrison: 50,
+            troops: 50,
+            defence: 1,
+            structures: 1,
+            image: 'sc/terrains/terrain.jpg'
+        },
+        'IT-42': {
+            name: 'Liguria',
+            capital: 'Genova',
+            income: 4,
+            population: 20,
+            neighbours: ['IT-21'],
+            army: 0,
+            garrison: 50,
+            troops: 50,
+            defence: 1,
+            structures: 1,
+            image: 'sc/terrains/terrain.jpg'
+        },
+        'IT-25': {
+            name: 'Lombardia',
+            capital: 'Milano',
+            income: 10,
+            population: 50,
+            neighbours: ['IT-21', 'IT-32'],
+            army: 0,
+            garrison: 50,
+            troops: 50,
+            defence: 1,
+            structures: 1,
+            image: 'sc/terrains/terrain.jpg'
+        },
+        'IT-32': {
+            name: 'Sud Tirolo',
+            capital: 'Trento',
+            income: 2,
+            population: 10,
+            neighbours: ['IT-25'],
+            army: 0,
+            garrison: 50,
+            troops: 50,
+            defence: 1,
+            structures: 1,
+            image: 'sc/terrains/terrain.jpg'
+        }
     }
 }
 
